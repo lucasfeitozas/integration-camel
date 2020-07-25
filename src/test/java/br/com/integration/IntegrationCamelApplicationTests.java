4dec37cbecfd8e05259ed48499963160665226aa
Lucas Feitozas <lucasfeitozas@gmail.com>
2020-07-25 15:12:32 -0300

    integration camel example

---
+++ src/test/java/br/com/integration/IntegrationCamelApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.integration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntegrationCamelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
